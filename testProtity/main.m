25c78b627f15e7cbed56b367e8009a5437619fd8
csjlengxiang <csjlengxiang@gmail.com>
2015-09-14 10:35:03 +0800

    Initial Commit

---
+++ testProtity/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testProtity
+//
+//  Created by csj on 15/9/14.
+//  Copyright © 2015年 csj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
